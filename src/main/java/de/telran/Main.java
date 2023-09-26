141d22c5a00d3ed2bfb74e265074af5e71591a42
Olena Andrieichenko <andreichenko.alyona@gmail.com>
2023-09-26 14:14:54 +0200

    Home Task1

--- src/main/java/de/telran/Main.java
+++ src/main/java/de/telran/Main.java
@@ -1,4 +1,6 @@
-package de.telran;public class Main {
+package de.telran;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ src/main/java/de/telran/lesson2/HomeTask1.java
@@ -0,0 +1,32 @@
+package de.telran.lesson2;
+
+public class HomeTask1 {
+    public static void main(String[] args) {
+
+        char c1 = 'G';
+        System.out.println("char: "+c1);
+
+        int arg1 = 89;
+        System.out.println("int: "+arg1);
+
+        byte myByte = 4;
+        System.out.println("byte: "+myByte);
+
+        short myShort = 56;
+        System.out.println("short: "+myShort);
+
+        float myFloat = (float) 4.7333436;
+        System.out.println("float: "+myFloat);
+
+        double myDouble = 4.355453532;
+        System.out.println("double: "+myDouble);
+
+        long Long = 12121;
+        System.out.println("long: "+Long);
+
+    }
+
+
+
+
+    }
