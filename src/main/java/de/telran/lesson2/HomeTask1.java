c95b53978c4b79767a91ee398b1acb9c934448f5
Olena Andrieichenko <andreichenko.alyona@gmail.com>
2023-09-27 15:40:47 +0200

    Home Task1+2

--- src/main/java/de/telran/lesson2/HomeTask1.java
+++ src/main/java/de/telran/lesson2/HomeTask1.java
@@ -1,4 +1,14 @@
 package de.telran.lesson2;
+//Задание 1
+//В методе main инициализировать все примитивные типы.
+//Вывести их результат в консоль:
+//char: G
+//int: 89
+//byte: 4
+//short: 56
+//float: 4.7333436
+//double: 4.355453532
+//long: 12121
 
 public class HomeTask1 {
     public static void main(String[] args) {
@@ -21,8 +31,8 @@ public class HomeTask1 {
         double myDouble = 4.355453532;
         System.out.println("double: "+myDouble);
 
-        long Long = 12121;
-        System.out.println("long: "+Long);
+        long myLong = 12121;
+        System.out.println("long: "+myLong);
 
     }
 
---
+++ src/main/java/de/telran/lesson2/HomeTask2.java
@@ -0,0 +1,21 @@
+package de.telran.lesson2;
+
+//* Дано трехзначное число. Вывести на экран все цифры этого числа
+//Пример: 345
+//Вывод в консоль: Число 345 -> 3, 4, 5
+
+public class HomeTask2 {
+    public static void main(String[] args) {
+        int arg = 345, arg1 = 100, arg2 = 10;
+        int div = arg / arg1;
+        int div1 = arg / arg2;
+        int ost = div1 % arg2;
+        int ost1 = arg % arg2;
+
+
+        System.out.print(div);
+        System.out.print(", "+ost);
+        System.out.print(", "+ost1);
+
+    }
+}
